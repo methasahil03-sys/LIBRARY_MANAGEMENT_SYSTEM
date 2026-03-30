e17a0a5ec40908c003fa65be7e10b64fd0e7d127
nagaraj <nagarajhegde174@gmail.com>
2026-03-30 22:42:19 +0530

    index

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+const express   = require("express");
+const cors      = require("cors");
+const mongoose  = require("mongoose");
+const dotenv    = require("dotenv");
+const path      = require("path");
